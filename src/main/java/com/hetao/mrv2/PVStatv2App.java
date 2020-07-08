367e61ae622781caf88b8d9d6e7de3c19afe3209
刘文豪 <35647418+hetaojun233@users.noreply.github.com>
2020-07-08 22:25:24 +0800

    Add files via upload
    经过ETL处理之后的数据，在进行数据分析

---
+++ src/main/java/com/hetao/mrv2/PVStatv2App.java
@@ -0,0 +1,76 @@
+package com.hetao.mrv2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class PVStatv2App {
+
+    static {
+        try {
+            System.load("F:/winutils-master/winutils-master/hadoop-3.0.0/bin/hadoop.dll");//建议采用绝对地址。
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Native code library failed to load.\n" + e);
+            System.exit(1);
+        }
+    }
+    public static void main(String[] args) throws Exception{
+
+        Configuration configuration=new Configuration();
+        FileSystem fileSystem=FileSystem.get(configuration);
+        Path outputPath=new Path(args[1]);
+        if (fileSystem.exists(outputPath)){
+            fileSystem.delete(outputPath,true);
+        }
+        Job job=Job.getInstance(configuration);
+
+        job.setJarByClass(PVStatv2App.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReduce.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(LongWritable.class);
+
+
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,outputPath);
+
+
+        job.waitForCompletion(true);
+    }
+
+    static class MyMapper extends Mapper<LongWritable, Text,Text,LongWritable>{
+
+        private  Text KEY=new Text("key");
+        private LongWritable ONE=new LongWritable(1);
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            context.write(KEY,ONE);;
+        }
+    }
+
+static  class MyReduce extends Reducer<Text,LongWritable, NullWritable,LongWritable>{
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+        long count=0;
+        for(LongWritable value:values){
+            count ++;
+        }
+        context.write(NullWritable.get(),new LongWritable(count));
+    }
+}
+}
---
+++ src/main/java/com/hetao/mrv2/ProvinceStatv2App.java
@@ -0,0 +1,94 @@
+package com.hetao.mrv2;
+
+import com.hetao.utils.IPParser;
+import com.hetao.utils.LogParser;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ProvinceStatv2App {
+    static {
+        try {
+            System.load("F:/winutils-master/winutils-master/hadoop-3.0.0/bin/hadoop.dll");//建议采用绝对地址。
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Native code library failed to load.\n" + e);
+            System.exit(1);
+        }
+    }
+    public static void main(String[] args) throws Exception{
+
+        Configuration configuration=new Configuration();
+
+        FileSystem fileSystem=FileSystem.get(configuration);
+        Path outputPath=new Path(args[1]);
+        if (fileSystem.exists(outputPath)){
+            fileSystem.delete(outputPath,true);
+        }
+
+        Job job=Job.getInstance(configuration);
+
+        job.setJarByClass(ProvinceStatv2App.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,outputPath);
+
+
+        job.waitForCompletion(true);
+    }
+
+    static class MyMapper extends Mapper<LongWritable,Text, Text,LongWritable>{
+
+
+        private LongWritable ONE=new LongWritable(1);
+
+        private LogParser logParser;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            logParser=new LogParser();
+        }
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String log =value.toString();
+
+            Map<String,String> info=logParser.parsev2(log);   //获取到信息
+            context.write(new Text(info.get("province")),ONE);
+
+        }
+    }
+
+    static class MyReducer extends Reducer <Text,LongWritable,Text,LongWritable>{
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long count=0;
+            for (LongWritable value:values){
+                count++;
+            }
+            context.write(key,new LongWritable(count));
+        }
+    }
+
+
+}
